23e361c21b284f8e2d50990c7959760303e119ee
Paurrev <paulreyes391@gmail.com>
2023-05-22 19:25:25 -0500

    Add status in the search

--- 1er-proyecto todo/src/TodoSearch.jsx
+++ 1er-proyecto todo/src/TodoSearch.jsx
@@ -1,17 +1,23 @@
+import { useState } from 'react';
 import './TodoSearch.css'
 
 export function TodoSearch (){
+  const [inputValue, setInputValue] = useState('')
+  
   const onSearchValueChange = (event) =>{
     console.log(event.target.value)
+    setInputValue(event.target.value)
   }
   return (
     <div className="todo-header">
       <input 
       className='todo-header_search' 
       placeholder="Create a new task..." 
+      value = {inputValue}
       onChange={onSearchValueChange}
+      autoFocus
       />
-      <span class="material-symbols-outlined todo-header_icon">search</span>
+      <span className="material-symbols-outlined todo-header_icon">search</span>
     </div>
   );
 }
