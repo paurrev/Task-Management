8d09ffc31d8047bd638674198bb9d2d67f36c2e5
Paurrev <paulreyes391@gmail.com>
2023-07-10 14:53:59 -0500

    Add useEffect and loading message and error

--- 1er-proyecto todo/src/App.jsx
+++ 1er-proyecto todo/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { TodoCounter } from './components/TodoCounter.jsx';
 import { TodoSearch } from './components/TodoSearch.jsx';
 import { TodoItem } from './components/TodoItem.jsx';
@@ -6,6 +6,7 @@ import { TodoList } from './components/TodoList.jsx';
 import { CreateTodoButton } from './components/CreateTodoButton.jsx';
 // import './App.css'
 
+// Creamos un array de ToDos por defecto
 const defaultTodos = [
   { text: 'Cortar cebolla', completed: false },
   { text: 'Tormar el curso de intro a react', completed: false },
@@ -14,23 +15,72 @@ const defaultTodos = [
   { text: 'Cantar', completed: false },
 ];
 
-export function App() {
-  // Traemos todos los ToDos almacenados
-  const localStorageTodos = localStorage.getItem('TODOS_V1');
-  // Iniicalizamos la variable de los ToDos
-  let parsedTodos;
+function useLocalStorage (itemName, initialValue){
+  // Creamos un estado para saber si hubo un error
+  const [error, setError] = useState(false);
+  // Creamos un estado para saber si estamos cargando los ToDos
+  const [loading, setLoading] = useState(true);
+  // Podemos utilizar otros hooks!!
+  const [items, setItem] = useState(initialValue);
 
-  if (!localStorageTodos) {
-    // Si el usuario es nuevo y no existe un item en localStorage, por lo tanto guardamos con un array vacio
-    localStorage.setItem('TODOS_V1', JSON.stringify([]));
-    parsedTodos = [];
-  } else {
-    // Si exite items en localStorage, transformamos el texto a objeto normal de ToDos
-    parsedTodos = JSON.parse(localStorageTodos);
+  
+  useEffect(() => {
+    setTimeout(() => {
+      try {
+        // Traemos todos los ToDos almacenados
+        const localStorageItems = localStorage.getItem(itemName);
+        // Iniicalizamos la variables de los ToDos
+        let parsedItems;
+      
+        if (!localStorageItems) {
+          // Si el usuario es nuevo y no existe un item en localStorage, por lo tanto guardamos con un array vacio
+          localStorage.setItem(itemName, JSON.stringify(initialValue));
+          parsedItems = [];
+        } else {
+          // Si exite items en localStorage, transformamos el texto a objeto normal de ToDos
+          parsedItems = JSON.parse(localStorageItems);
+        }
+  
+        setItem(parsedItems)
+        setLoading(false)
+      } catch (error) {
+        setError(error)
+      }
+    }, 1000);
+  })
+
+
+
+  // Logica para guardar los ToDos en localStorage
+  const saveItems = (newItems) => {
+    try {
+      // Convertimos a String nuestros ToDos
+      const stringifyItems = JSON.stringify(newItems);
+      // Guardamos en localStorage
+      localStorage.setItem(itemName, stringifyItems);
+      // Actualizamos el estado de los ToDos
+      setItem(newItems);
+    } catch (error) {
+      setError(error)
+    }
+  };
+
+  return{ 
+    items,
+    saveItems,
+    loading,
+    error
   }
+}
 
-  // Estado inicial de nuestros ToDos
-  const [todos, setTodos] = useState(parsedTodos);
+export function App() {
+  
+  const {
+    items: todos,
+    saveItems: saveTodos,
+    loading,
+    error
+  } = useLocalStorage('TODOS_V1', []);
   // Estado inicial de la búsqueda
   const [inputValue, setInputValue] = useState('');
   // Cantidad de ToDos completados
@@ -52,21 +102,12 @@ export function App() {
     });
   }
 
-  // Logica para guardar los ToDos en localStorage
-  const saveTodos = (newTodos) => {
-    // Convertimos a String nuestros ToDos
-    const stringifyTodos = JSON.stringify(newTodos);
-    // Guardamos en localStorage
-    localStorage.setItem('TODOS_V1', stringifyTodos);
-    // Actualizamos el estado de los ToDos
-    setTodos(newTodos);
-  };
 
   //Logica para marcar como completado un ToDo
   const completeTodo = (text) => {
     const todoIndex = todos.findIndex((todo) => todo.text === text);
     const newTodos = [...todos];
-    newTodos[todoIndex].completed = true;
+    newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
     saveTodos(newTodos);
   };
 
@@ -83,6 +124,10 @@ export function App() {
       <TodoSearch inputValue={inputValue} setInputValue={setInputValue} />
       <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
       <TodoList>
+
+        {error && <p>Desespérate, hubo un error 😢</p>}
+        {loading && <p>Estamos cargando... ⌛</p>}
+        {(!loading && !searchedTodos.length) && <p>Crea tu primer TODO ✏️</p>}      
         {searchedTodos.map((todo) => (
           <TodoItem
             key={todo.text}
