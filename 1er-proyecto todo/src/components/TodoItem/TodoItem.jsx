479892da773b84bcc4c4a4f7a15755df6bb59fa0
paurrev <paulreyes391@gmail.com>
2024-09-03 20:48:19 -0500

    Add new elements and budgets to Todo Items

--- 1er-proyecto todo/src/components/TodoItem/TodoItem.jsx
+++ 1er-proyecto todo/src/components/TodoItem/TodoItem.jsx
@@ -1,10 +1,77 @@
 import './TodoItem.css';
-import { IconBin } from '../../assets/Icons';
+import {
+  IconBin,
+  IconCalendar,
+  IconCircleCheck,
+  IconHighPriority,
+  IconLowPriority,
+  IconMediumPriority,
+  IconProgress,
+  IconTodo,
+} from '../../assets/Icons';
+import { TodoBadge } from '../TodoBadge/TodoBadge';
+import '../../variables.css';
 
-export function TodoItem({ onCompleted, completed, text, onDeleted }) {
+export function TodoItem({
+  onCompleted,
+  completed,
+  textTodo,
+  onDeleted,
+  status,
+  priority,
+  dueDate,
+}) {
+  const statusMap = {
+    todo: {
+      icon: <IconTodo size={20} color="var(--layout-foreground)" />,
+      text: 'Todo',
+    },
+    'in-progress': {
+      icon: <IconProgress size={20} color="var(--layout-foreground)" />,
+      text: 'In Progress',
+    },
+    completed: {
+      icon: <IconCircleCheck size={20} color="var(--layout-foreground)" />,
+      text: 'Completed',
+    },
+  };
+
+  const priorityMap = {
+    low: {
+      icon: (
+        <IconLowPriority
+          size={24}
+          stroke={2}
+          color="var(--layout-foreground)"
+        />
+      ),
+      text: 'Low',
+    },
+    medium: {
+      icon: (
+        <IconMediumPriority
+          size={24}
+          stroke={2}
+          color="var(--layout-foreground)"
+        />
+      ),
+      text: 'Medium',
+    },
+    high: {
+      icon: (
+        <IconHighPriority
+          size={24}
+          stroke={2}
+          color="var(--layout-foreground)"
+        />
+      ),
+      text: 'High',
+    },
+  };
+
   return (
     <li className="todo-item">
-      <div className='Todo-item--todo-task'>
+      <div className="Todo-item--todo-task">
         <label className={`container ${completed ? 'checked' : ''}`}>
           <input
             type="checkbox"
@@ -18,13 +85,32 @@ export function TodoItem({ onCompleted, completed, text, onDeleted }) {
             completed && 'todo-item_text--completed'
           }`}
         >
-          {text}
+          {textTodo}
         </p>
       </div>
-
-      <button className="todo-item_delete" onClick={onDeleted}>
-        <IconBin />
-      </button>
+      <div className="todo-item--badges">
+        <TodoBadge
+          classContainer="status-badges"
+          className={status}
+          icon={statusMap[status]?.icon}
+          valueText={statusMap[status]?.text}
+        />
+        <TodoBadge
+          classContainer="priority-badges"
+          className={priority}
+          icon={priorityMap[priority]?.icon}
+          valueText={priorityMap[priority]?.text}
+        />
+        <TodoBadge
+          classContainer="date-badges"
+          className="date"
+          icon={<IconCalendar size={24} color="var(--layout-foreground)" />}
+          valueText={dueDate}
+        />
+        <button className="todo-item_delete" onClick={onDeleted}>
+          <IconBin />
+        </button>
+      </div>
     </li>
   );
 }
