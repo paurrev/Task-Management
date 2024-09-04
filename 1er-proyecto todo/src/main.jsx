c240b38d581d7681d72f8dabb570e8d376423628
paurrev <paulreyes391@gmail.com>
2024-09-03 20:36:31 -0500

    Add StrictMode to main.jsx

--- 1er-proyecto todo/src/main.jsx
+++ 1er-proyecto todo/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import { App } from './App/App.jsx'
 import '@fontsource-variable/heebo';
 import './index.css'
+import { StrictMode } from 'react';
 
 const root = createRoot(document.getElementById('root'))
 
 root.render(
-  <App />
+  <StrictMode>
+    <App />
+  </StrictMode>
 )
