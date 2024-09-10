99ac183d9070f3ffef747515849d40417d82fa44
paurrev <paulreyes391@gmail.com>
2024-09-09 23:35:12 -0500

    Change size of checkbox

--- src/components/TodoItem/TodoItem.jsx
+++ src/components/TodoItem/TodoItem.jsx
@@ -87,17 +87,16 @@ export function TodoItem({
             <svg width="45" height="45" viewBox="0 0 95 95">
               <rect
                 x="30"
-                y="15"
-                width="60"
-                height="60"
+                y="20"
+                width="50"
+                height="50"
                 stroke="currentColor"
                 fill="none"
                 stroke-width="3"
                 className="square"
-                rx="15"
-                ry="15"
+                rx="15" ry="15"
               ></rect>
-              <g transform="translate(12,-955.36222)">
+              <g transform="translate(6,-955.36222)">
                 <path
                   d="m 56,963 c -102,122 6,9 7,9 17,-5 -66,69 -38,52 122,-77 -7,14 18,4 29,-11 45,-43 23,-4"
                   stroke="#14B858"
